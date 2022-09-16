9af5a5cde88c36e906dd33c9e52c956f93678782
Seunghwan <ysp1307@naver.com>
2022-09-16 14:34:41 +0900

    ✨ feat : 메일 찾기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import "./App.css";
-
+import { data } from "./data/data";
 function App() {
-  return <div className="App"></div>;
+  const [userId, setUserId] = useState("");
+  let [userEmail, setUserEmail] = useState("");
+
+  const findUserEmail = (userId) => {
+    let result = data.find((a) => a.name === userId);
+    if (!userId || result === undefined) {
+      setUserEmail("잘못 입력하셨거나 없는 이름입니다.");
+      return;
+    }
+    setUserEmail(result.email);
+  };
+  const handleInput = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const handleOnClick = () => {
+    findUserEmail(userId);
+  };
+
+  const handleOnKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleOnClick();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">페어찾기!</div>
+      <div>찾으시는 페어분의 성함을 적어주세요!</div>
+      <input
+        type="text"
+        value={userId}
+        onChange={handleInput}
+        onKeyPress={handleOnKeyPress}
+      ></input>
+      <label>
+        <button onClick={handleOnClick}> 검색!</button>
+      </label>
+      <div className="resultBox">{userEmail}</div>
+    </div>
+  );
 }
 
 export default App;
